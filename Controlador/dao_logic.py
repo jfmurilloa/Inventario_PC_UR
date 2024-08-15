aef988c3d26157cea207ef2f5d0097c120a39286
jfmurilloa <jfmurilloa@misena.edu.co>
2024-08-15 13:25:31 -0500

    agosto-15-1

--- Controlador/dao_logic.py
+++ Controlador/dao_logic.py
@@ -3,11 +3,14 @@ import mysql.connector
 from mysql.connector import Error
 from tkinter import messagebox
 from Modelo.owner import *
+from Modelo.location import *
 
 
 class Dao:
     def __init__(self,db):
         self.db=db
+    
+    #CRUD Cuentadante
     
     def crear_cuentadante(self,obj:Cuentadante):
         val=(obj.documento,obj.nombres,obj.apellidos,obj.correo,obj.celular)
@@ -45,6 +48,53 @@ class Dao:
         valor= messagebox.askquestion('Eliminar',mensaje)
         if valor == 'yes':
             delete='delete from cuentadante where id= %s'
+            try:
+                self.db.cursor.execute(delete,(obj.id,))
+                self.db.connection.commit()
+                messagebox.showinfo('Eliminar','El registro ha sido eliminado...')
+                return True
+            except mysql.connector.Error as e:
+                messagebox.showinfo('Eliminar',e)
+                return False
+    
+    #CRUD Ubicacion
+    
+    def crear_ubicacion(self,obj:Ubicacion):
+        val=(obj.nombre,obj.descripcion)
+        insert='insert into ubicacion(nombre,descripcion) values (%s,%s)'
+        try:
+            self.db.cursor.execute(insert,val)
+            self.db.connection.commit()
+            messagebox.showinfo('Nuevo Registro','La ubicación de ha almacenado...')
+        except mysql.connector.Error as e:
+            messagebox.showinfo('Nuevo Registro',e)
+    
+    def buscar_ubicacion(self,nom):
+        criterio=(nom,)
+        select='select * from ubicacion where nombre= %s'
+        try:
+            self.db.cursor.execute(select,criterio)
+            obj_ubi= self.db.cursor.fetchone()
+            return obj_ubi
+        except mysql.connector.Error as e:
+            messagebox.showerror('Error',e)
+            return None
+    
+    def modificar_ubicacion(self,obj:Ubicacion):
+        val=(obj.nombre,obj.descripcion,obj.id)
+        update='update ubicacion set nombre= %s, descripcion=%s where id= %s'
+        try:
+            self.db.cursor.execute(update,val)
+            self.db.connection.commit()
+            messagebox.showinfo('Update','El registro ha sido modificado...')
+        except mysql.connector.Error as e:
+            messagebox.showerror('Update',e)
+        
+    def eliminar_ubicacion(self,obj:Ubicacion):
+        mensaje='Esta seguro de eliminar el registro?'
+        valor= messagebox.askquestion('Eliminar',mensaje)
+        if valor == 'yes':
+            delete='delete from ubicacion where id= %s'
             try:
                 self.db.cursor.execute(delete,(obj.id,))
                 self.db.connection.commit()
--- Vista/gui.py
+++ Vista/gui.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 from Modelo.owner import *
+from Modelo.location import *
 from Controlador.conexion import *
 from Controlador.dao_logic import *
 
@@ -26,12 +27,16 @@ class MiVentanaPrincipal:
         self.cuentadanteMenu.add_command(label='Admon_Cuentadantes',command=self.frm_cuentadante)
         self.cuentadanteMenu.add_command(label='Salir',command=self.salir)
 
+        self.ubicacionMenu=Menu(self.barraMenu,tearoff=0)
+        self.ubicacionMenu.add_command(label='Admon_Ubicaciones',command=self.frm_ubicacion)
+
         self.ayudaMenu=Menu(self.barraMenu,tearoff=0)
         self.ayudaMenu.add_command(label='Acerca de...')
         self.ayudaMenu.add_command(label='Licencia')
 
         #Agregar opciones a los menus
         self.barraMenu.add_cascade(label='Cuentadantes',menu=self.cuentadanteMenu)
+        self.barraMenu.add_cascade(label='Ubicaciones',menu=self.ubicacionMenu)
         self.barraMenu.add_cascade(label='Ayuda',menu=self.ayudaMenu)
 
         #Variables vinculadas a los Entry
@@ -176,6 +181,95 @@ class MiVentanaPrincipal:
         self.caja5.set('')
         self.caja6.set('')
         self.txt_caja2.focus()
+    
+    #Formulario para ubicacion
+    def frm_ubicacion(self):
+        ventana= Toplevel(self.root)
+        ventana.title('Administración de ubicaciones')
+        ventana.config(width=500,height=500)
+
+        # Para los controles se adapten mejor a la ventana
+        ventana.columnconfigure(0, weight=1)
+        ventana.rowconfigure(0, weight=25)
+        ventana.columnconfigure(1, weight=2)
+        ventana.rowconfigure(1, weight=1)
+
+        frame1= Frame(ventana,bg='gray15')
+        frame1.grid(row=0,column=0, sticky='nsew')
+
+        frame2=Frame(ventana,bg='CadetBlue1')
+        frame2.grid(row=1,columnspan=1, sticky='nsew')
+
+        
+        lbl_id=Label(frame1,text='Id',width=15)
+        lbl_id.grid(row=0,column=0,padx=10,pady=10)
+        self.txt_caja1=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja1,state='readonly')
+        self.txt_caja1.grid(row=0,column=1)
+
+        lbl_nombre=Label(frame1,text='Nombre',width=15)
+        lbl_nombre.grid(row=1,column=0,padx=10,pady=10)
+        self.txt_caja2=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja2)
+        self.txt_caja2.grid(row=1,column=1)        
+
+        lbl_descripcion=Label(frame1,text='Descripcion',width=15)
+        lbl_descripcion.grid(row=2,column=0,padx=10,pady=10)
+        self.txt_caja3=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja3)
+        self.txt_caja3.grid(row=2,column=1)        
+        
+        #Colocar los botones en frame 2
+        self.btn_nuevo= Button(frame2,width=10,font=('Arial',12,'bold'),text='Nuevo',bg='orange',bd=5,command=self.crear_ubicacion_v)
+        self.btn_nuevo.grid(row=0,column=0,padx=10,pady=10)
+
+        self.btn_buscar= Button(frame2,width=10,font=('Arial',12,'bold'),text='Buscar',bg='orange',bd=5,command=self.buscar_ubicacion_v)
+        self.btn_buscar.grid(row=0,column=1,padx=10,pady=10)
+        self.btn_modificar= Button(frame2,width=10,font=('Arial',12,'bold'),text='Modificar',bg='orange',bd=5,command=self.modificar_ubicacion_v)
+        self.btn_modificar.grid(row=0,column=2,padx=10,pady=10)
+
+        self.btn_eliminar= Button(frame2,width=10,font=('Arial',12,'bold'),text='Eliminar',bg='orange',bd=5,command=self.eliminar_ubicacion_v)
+        self.btn_eliminar.grid(row=0,column=3,padx=10,pady=10)
+
+        ventana.focus()
+        self.txt_caja2.focus()
+        ventana.grab_set()
+    
+    def crear_ubicacion_v(self):
+        if self.caja2.get() and self.caja3.get() !='':
+            obj_ubicacion=Ubicacion(self.caja2.get(),self.caja3.get())
+            dao.crear_ubicacion(obj_ubicacion)
+            self.limpiar()
+        else:
+            messagebox.showerror('Error','Todos los campos son obligatorios...')
+    
+    def buscar_ubicacion_v(self):
+        if self.caja2.get() != '':
+            ubicacion= dao.buscar_ubicacion(self.caja2.get())
+            if ubicacion != None:
+                obj_ubi=Ubicacion(ubicacion[1],ubicacion[2],ubicacion[0])
+                self.caja1.set(obj_ubi.id)
+                self.caja2.set(obj_ubi.nombre)
+                self.caja3.set(obj_ubi.descripcion)
+                
+            else:
+                messagebox.showwarning('No encontrado','Registro no encontrado...')
+        else:
+            messagebox.showwarning('No encontrado','debe enviar un criterio de busqueda...')
+
+    def modificar_ubicacion_v(self):
+        if self.caja1.get() and self.caja2.get() and self.caja3.get()!='':
+            obj_ubicacion= Ubicacion(self.caja2.get(),self.caja3.get(),self.caja1.get())
+            dao.modificar_ubicacion(obj_ubicacion)
+            self.limpiar()
+        else:
+            messagebox.showwarning('Error','Todos los campos son obligatorios...')
+
+    def eliminar_ubicacion_v(self):
+        if self.caja1.get() and self.caja2.get() and self.caja3.get() !='':
+            obj_ubicacion= Ubicacion(self.caja2.get(),self.caja3.get(),self.caja1.get())
+            res=dao.eliminar_ubicacion(obj_ubicacion)
+            if res:
+                self.limpiar()
+        else:
+            messagebox.showwarning('Error','Todos los campos son obligatorios...')
     
     
     
