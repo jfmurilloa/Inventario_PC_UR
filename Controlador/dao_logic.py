df9ffe38ff4846e35d75892c8476d8417dfaa708
jfmurilloa <jfmurilloa@misena.edu.co>
2024-08-15 20:51:08 -0500

    clase agosto 15

--- Controlador/dao_logic.py
+++ Controlador/dao_logic.py
@@ -4,6 +4,7 @@ from mysql.connector import Error
 from tkinter import messagebox
 from Modelo.owner import *
 from Modelo.location import *
+from Modelo.supplier import *
 
 
 class Dao:
@@ -95,6 +96,53 @@ class Dao:
         valor= messagebox.askquestion('Eliminar',mensaje)
         if valor == 'yes':
             delete='delete from ubicacion where id= %s'
+            try:
+                self.db.cursor.execute(delete,(obj.id,))
+                self.db.connection.commit()
+                messagebox.showinfo('Eliminar','El registro ha sido eliminado...')
+                return True
+            except mysql.connector.Error as e:
+                messagebox.showinfo('Eliminar',e)
+                return False
+    
+    #CRUD Proveedor
+    
+    def crear_proveedor(self,obj:Proveedor):
+        val=(obj.nit,obj.razon_social,obj.direccion,obj.telefono,obj.email)
+        insert='insert into proveedor(nit,razon_social,direccion,telefono,email) values (%s,%s,%s,%s,%s)'
+        try:
+            self.db.cursor.execute(insert,val)
+            self.db.connection.commit()
+            messagebox.showinfo('Nuevo Registro','El proveedor ha sido almacenado...')
+        except mysql.connector.Error as e:
+            messagebox.showinfo('Nuevo Registro',e)
+    
+    def buscar_proveedor(self,nit):
+        criterio=(nit,)
+        select='select * from proveedor where nit= %s'
+        try:
+            self.db.cursor.execute(select,criterio)
+            obj_pro= self.db.cursor.fetchone()
+            return obj_pro
+        except mysql.connector.Error as e:
+            messagebox.showerror('Error',e)
+            return None
+    
+    def modificar_proveedor(self,obj:Proveedor):
+        val=(obj.nit,obj.razon_social,obj.direccion,obj.telefono,obj.email,obj.id)
+        update='update proveedor set nit= %s, razon_social= %s, direccion=%s, telefono=%s, email=%s where id= %s'
+        try:
+            self.db.cursor.execute(update,val)
+            self.db.connection.commit()
+            messagebox.showinfo('Update','El registro ha sido modificado...')
+        except mysql.connector.Error as e:
+            messagebox.showerror('Update',e)
+        
+    def eliminar_proveedor(self,obj:Proveedor):
+        mensaje='Esta seguro de eliminar el registro?'
+        valor= messagebox.askquestion('Eliminar',mensaje)
+        if valor == 'yes':
+            delete='delete from proveedor where id= %s'
             try:
                 self.db.cursor.execute(delete,(obj.id,))
                 self.db.connection.commit()
--- Vista/gui.py
+++ Vista/gui.py
@@ -30,12 +30,16 @@ class MiVentanaPrincipal:
         self.ubicacionMenu=Menu(self.barraMenu,tearoff=0)
         self.ubicacionMenu.add_command(label='Admon_Ubicaciones',command=self.frm_ubicacion)
 
+        self.proveedorMenu=Menu(self.barraMenu,tearoff=0)
+        self.proveedorMenu.add_command(label='Admon_Proveedores',command=self.frm_proveedor)
+
         self.ayudaMenu=Menu(self.barraMenu,tearoff=0)
         self.ayudaMenu.add_command(label='Acerca de...')
         self.ayudaMenu.add_command(label='Licencia')
 
         #Agregar opciones a los menus
         self.barraMenu.add_cascade(label='Cuentadantes',menu=self.cuentadanteMenu)
+        self.barraMenu.add_cascade(label='Proveedores',menu=self.proveedorMenu)
         self.barraMenu.add_cascade(label='Ubicaciones',menu=self.ubicacionMenu)
         self.barraMenu.add_cascade(label='Ayuda',menu=self.ayudaMenu)
 
@@ -266,6 +270,113 @@ class MiVentanaPrincipal:
         if self.caja1.get() and self.caja2.get() and self.caja3.get() !='':
             obj_ubicacion= Ubicacion(self.caja2.get(),self.caja3.get(),self.caja1.get())
             res=dao.eliminar_ubicacion(obj_ubicacion)
+            if res:
+                self.limpiar()
+        else:
+            messagebox.showwarning('Error','Todos los campos son obligatorios...')
+
+    def frm_proveedor(self):
+        ventana= Toplevel(self.root)
+        ventana.title('Administración de proveedores')
+        ventana.config(width=500,height=500)
+
+        # Para los controles se adapten mejor a la ventana
+        ventana.columnconfigure(0, weight=1)
+        ventana.rowconfigure(0, weight=25)
+        ventana.columnconfigure(1, weight=2)
+        ventana.rowconfigure(1, weight=1)
+
+        frame1= Frame(ventana,bg='gray15')
+        frame1.grid(row=0,column=0, sticky='nsew')
+
+        frame2=Frame(ventana,bg='CadetBlue1')
+        frame2.grid(row=1,columnspan=1, sticky='nsew')
+
+        
+        lbl_id=Label(frame1,text='Id',width=15)
+        lbl_id.grid(row=0,column=0,padx=10,pady=10)
+        self.txt_caja1=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja1,state='readonly')
+        self.txt_caja1.grid(row=0,column=1)
+
+        lbl_nit=Label(frame1,text='Nit',width=15)
+        lbl_nit.grid(row=1,column=0,padx=10,pady=10)
+        self.txt_caja2=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja2)
+        self.txt_caja2.grid(row=1,column=1)
+                
+
+        lbl_razon_social=Label(frame1,text='Razon Social',width=15)
+        lbl_razon_social.grid(row=2,column=0,padx=10,pady=10)
+        self.txt_caja3=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja3)
+        self.txt_caja3.grid(row=2,column=1)
+
+        lbl_direccion=Label(frame1,text='Direccion',width=15)
+        lbl_direccion.grid(row=3,column=0,padx=10,pady=10)
+        self.txt_caja4=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja4)
+        self.txt_caja4.grid(row=3,column=1)
+
+        lbl_telefono=Label(frame1,text='Telefono',width=15)
+        lbl_telefono.grid(row=4,column=0,padx=10,pady=10)
+        self.txt_caja5=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja5)
+        self.txt_caja5.grid(row=4,column=1)
+
+        lbl_email=Label(frame1,text='Email',width=15)
+        lbl_email.grid(row=5,column=0,padx=10,pady=10)
+        self.txt_caja6=Entry(frame1,width=20,font=('Arial',12),textvariable=self.caja6)
+        self.txt_caja6.grid(row=5,column=1)
+        
+        #Colocar los botones en frame 2
+        self.btn_nuevo= Button(frame2,width=10,font=('Arial',12,'bold'),text='Nuevo',bg='orange',bd=5,command=self.crear_proveedor_v)
+        self.btn_nuevo.grid(row=0,column=0,padx=10,pady=10)
+
+        self.btn_buscar= Button(frame2,width=10,font=('Arial',12,'bold'),text='Buscar',bg='orange',bd=5,command=self.buscar_proveedor_v)
+        self.btn_buscar.grid(row=0,column=1,padx=10,pady=10)
+
+        self.btn_modificar= Button(frame2,width=10,font=('Arial',12,'bold'),text='Modificar',bg='orange',bd=5,command=self.modificar_proveedor_v)
+        self.btn_modificar.grid(row=0,column=2,padx=10,pady=10)
+
+        self.btn_eliminar= Button(frame2,width=10,font=('Arial',12,'bold'),text='Eliminar',bg='orange',bd=5,command=self.eliminar_proveedor_v)
+        self.btn_eliminar.grid(row=0,column=3,padx=10,pady=10)
+
+        ventana.focus()
+        self.txt_caja2.focus()
+        ventana.grab_set()
+    
+    def crear_proveedor_v(self):
+        if self.caja2.get() and self.caja3.get() and self.caja4.get() and self.caja5.get() and self.caja6.get() !='':
+            obj_proveedor=Proveedor(self.caja2.get(),self.caja3.get(),self.caja4.get(),self.caja5.get(),self.caja6.get())
+            dao.crear_proveedor(obj_proveedor)
+            self.limpiar()
+        else:
+            messagebox.showerror('Error','Todos los campos son obligatorios...')
+    
+    def buscar_proveedor_v(self):
+        if self.caja2.get() != '':
+            proveedor= dao.buscar_proveedor(self.caja2.get())
+            if proveedor != None:
+                obj_pro=Proveedor(proveedor[1],proveedor[2],proveedor[3],proveedor[4],proveedor[5],proveedor[0])
+                self.caja1.set(obj_pro.id)
+                self.caja2.set(obj_pro.nit)
+                self.caja3.set(obj_pro.razon_social)
+                self.caja4.set(obj_pro.direccion)
+                self.caja5.set(obj_pro.telefono)
+                self.caja6.set(obj_pro.email)
+            else:
+                messagebox.showwarning('No encontrado','Registro no encontrado...')
+        else:
+            messagebox.showwarning('No encontrado','debe enviar un criterio de busqueda...')
+
+    def modificar_proveedor_v(self):
+        if self.caja1.get() and self.caja2.get() and self.caja3.get() and self.caja4.get() and self.caja5.get() and self.caja6.get() !='':
+            obj_proveedor= Proveedor(self.caja2.get(),self.caja3.get(),self.caja4.get(),self.caja5.get(),self.caja6.get(),self.caja1.get())
+            dao.modificar_proveedor(obj_proveedor)
+            self.limpiar()
+        else:
+            messagebox.showwarning('Error','Todos los campos son obligatorios...')
+
+    def eliminar_proveedor_v(self):
+        if self.caja1.get() and self.caja2.get() and self.caja3.get() and self.caja4.get() and self.caja5.get() and self.caja6.get() !='':
+            obj_proveedor= Proveedor(self.caja2.get(),self.caja3.get(),self.caja4.get(),self.caja5.get(),self.caja6.get(),self.caja1.get())
+            res=dao.eliminar_proveedor(obj_proveedor)
             if res:
                 self.limpiar()
         else:
